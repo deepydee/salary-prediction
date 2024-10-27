7249793149671d522039b53ea3381a4ee09c5a78
Vitalii Semenov <v.semenov@astrio.net>
2024-10-27 23:09:37 +0500

    filter out languages and cities

--- real-life/main.py
+++ real-life/main.py
@@ -5,9 +5,18 @@ import seaborn as sns
 
 df = pd.read_csv('salaries-2023.csv')
 df = df.dropna()
-print(df.head())
+
+allowed_languages = ['php', 'js', '.net', 'java']
+df = df[df['language'].isin(allowed_languages)]
+
+vilnius_names = ['Vilniuj', 'Vilniua', 'VILNIUJE', 'VILNIUS', 'vilnius', 'Vilniuje']
+condition = df['city'].isin(vilnius_names)
+df.loc[condition, 'city'] = 'Vilnius'
+
+kaunas_names = ['KAUNAS', 'kaunas', 'Kaune']
+condition = df['city'].isin(kaunas_names)
+df.loc[condition, 'city'] = 'Kaunas'
+
+allowed_cities = ['Vilnius', 'Kaunas']
+df = df[df['city'].isin(allowed_cities)]
 print(df.shape)
-
-df.info()
-
-print(df.describe())
